eba8490cfe8ea24f371bda2ef6f1a68ac4d922c7
Koushik Panchadarla <koushikpanchadarla@gmail.com>
2026-01-23 19:43:26 +0530

    feat: integrate node_modules obfuscation scanning into ForgeScan

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -5,20 +5,33 @@ mod report;
 mod package_json;
 mod loader;
 mod scanner;
+mod finding;
+mod fs_scan;
+mod obfuscation;
+mod node_modules_scan;
 
 use loader::load_package_json;
 use scanner::scan_dependencies;
 use report::print_warning;
+use node_modules_scan::scan_node_modules;
 
 fn main() {
     let pkg = load_package_json("../samples/package.json");
 
     if let Some(dependencies) = pkg.dependencies {
-        let dep_names = dependencies.keys().cloned().collect();
+        let dep_names: Vec<String> = dependencies.keys().cloned().collect();
         let findings = scan_dependencies(&dep_names);
 
         for finding in findings {
             print_warning(&finding);
         }
+    }
+
+    println!("\nScanning node_modules for obfuscated malware...\n");
+
+    let malware_findings = scan_node_modules("../node_modules");
+
+    for finding in malware_findings {
+        println!("{:?}", finding);
     }
 }
